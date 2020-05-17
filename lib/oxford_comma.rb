37e8035353ac8cb2042249c3901e04e53ad59192
Janis Lebron <janislebron@gmail.com>
2020-05-17 20:26:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,11 @@
 def oxford_comma(array)
 #if two elements in the array, join elements with and
-  if element = 2
-    array.join(" and ")
+if array.size == 1
+  return array[0]
+elsif array.size == 2
+  return array.join(" and ")
 #if three or more elements in the array, join elements with comma, but push and before the last element
-  else element >=3
-    array.join(", ")
+else
+  return array[0..-2].join(', ') + ", and " + array[-1]
+  end
 end
